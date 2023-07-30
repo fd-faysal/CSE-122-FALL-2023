d77d6a3b2cc22644a8b9fa274c4b0e0fda814e30
Faysal Islam <108185618+fd-faysal@users.noreply.github.com>
2023-07-30 15:04:37 +0600

    update

---
+++ LAB-01/1st.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+cout<<"Welcome to the wonderful world of C++!!!"<<endl;
+cout<<"Lab1\t";
+cout<<"\nEnd \t PROGRAM\t BYE";
+cout<<"Welcome in yarmouk university\rHellooo";
+return 0;
+}
+
+
+
+/*
+/r = CARRIAGE RETURN
+*/
---
+++ LAB-01/2nd.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"Hello in my university"<<endl<<endl;
+    cout<<"Im a student in it faculty"<<endl<<endl<<"Welcome"<<endl;
+return 0;
+}
