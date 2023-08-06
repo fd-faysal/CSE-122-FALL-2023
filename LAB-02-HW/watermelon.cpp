2428ed64807da1d97974dac42c11738fa89b0075
Faysal Islam <108185618+fd-faysal@users.noreply.github.com>
2023-08-06 14:57:32 +0600

    Update watermelon.cpp

--- LAB-02-HW/watermelon.cpp
+++ LAB-02-HW/watermelon.cpp
@@ -1,13 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
 
 #include<stdio.h>
-int main(){
- 
+
 int w;
-scanf("%d", &w);
+cin>>w;
 if(w%2 == 0 && w > 2){
   printf("YES\n");
 } else{
   printf("NO\n");
 }
-  return 0;
+return 0;
 }
