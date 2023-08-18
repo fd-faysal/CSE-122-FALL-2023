a7967bae4ff5949d13bce7bfdf9fd6fa51c13d1f
Faysal Islam <108185618+fd-faysal@users.noreply.github.com>
2023-08-18 18:10:20 +0600

    Add files via upload

---
+++ LAB-04/A_Beautiful_Matrix.cpp
@@ -0,0 +1,23 @@
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,count;
+ 
+
+ for(int i=1;i<=5;i++){
+    for(int j=1;j<=5;j++){
+        cin >> n;
+        if(n==1){
+          count=abs(i-3)+abs(j-3);  
+        }
+
+    }
+ }
+
+   cout<<count;
+   return 0;
+}   
